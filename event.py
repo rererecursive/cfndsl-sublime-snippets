5b5611a5484353a325096fae493865a2ecda96dd
zac <z.t.lewis@lavabit.com>
2020-02-07 22:31:50 +1100

    Add snippets and pop-ups for intrinsic functions.

--- event.py
+++ event.py
@@ -2,15 +2,34 @@ import math
 import os
 import sublime
 import sublime_plugin
+import webbrowser
 from xml.dom import minidom
 
 class CfndslListener(sublime_plugin.EventListener):
     def on_hover(self, view, point, hover_zone):
         selected_word = view.substr(view.word(point))
-
         filename = '%s.sublime-snippet' % selected_word
-        directory = os.path.dirname(os.path.realpath(__file__)) + '/snippets'
         attributes = {}
+
+        # Function pop-up
+        if selected_word in [
+                'Condition', 'FnAnd', 'FnBase64', 'FnCidr', 'FnEquals',
+                'FnFindInMap', 'FnGetAtt', 'FnGetAZs', 'FnIf', 'FnImportValue',
+                'FnJoin', 'FnNot', 'FnOr', 'FnSelect', 'FnSplit', 'FnSub']:
+            directory = os.path.dirname(os.path.realpath(__file__)) + '/fn-snippets'
+            doc = minidom.parse(directory + '/' + filename)
+            url = doc.getElementsByTagName('url')[0].firstChild.nodeValue.strip()
+            example = doc.getElementsByTagName('example')[0].firstChild.nodeValue.strip()
+
+            if '\n' in example:
+                example = '<br>'.join(example.split('\n'))
+
+            popup = self.construct_function_popup(selected_word, example, url)
+            self.show_popup(popup, view, point)
+            return
+
+        # Resource pop-up
+        directory = os.path.dirname(os.path.realpath(__file__)) + '/snippets'
 
         if filename in os.listdir(directory):
             doc = minidom.parse(directory + '/' + filename)
@@ -24,9 +43,37 @@ class CfndslListener(sublime_plugin.EventListener):
 
         #print("attrs:", attributes)
         if attributes:
-            self.show_popup(view, point, attributes, selected_word)
+            popup = self.construct_resource_popup(selected_word, attributes)
+            self.show_popup(popup, view, point)
 
-    def show_popup(self, view, point, resource_attributes, resource_name):
+    def construct_function_popup(self, function_name, example, url):
+        """Construct the function pop-up.
+
+        Produces (unformatted):
+
+            FnAnd
+            -----
+            Example:
+                FnAnd(FnEquals(...), FnNot(...))
+        """
+        text = '<b><a href="%s"><u>%s</u></a></b> (Intrinsic function)<br><br>' % (url, function_name)
+        text += '<b>Example:</b><br>'
+        text += ' %s' % example
+
+        return text
+
+
+    def construct_resource_popup(self, resource_name, resource_attributes):
+        """Construct the resource pop-up.
+
+        Produces (unformatted):
+
+            EC2_VPC
+            -------
+            Outputs:
+                SomeOutput1    String
+                SomeOutput2   [String]
+        """
         text = '<b><u>%s</u></b><br>' % resource_name
         text += '<b>Outputs:</b><br>'
 
@@ -46,6 +93,12 @@ class CfndslListener(sublime_plugin.EventListener):
                 spaces = ' '
             text += ' %s%s<i>%s</i><br>' % (attr_name, spaces, attr_type)
 
+        return text
+
+    def show_popup(self, popup, view, point):
         # Display the popup on the line below the cursor
         (row, col) = view.rowcol(point)
-        view.show_popup(text, on_navigate=print, location=view.text_point(row,col))
+        view.show_popup(popup, max_width=700, on_navigate=open_url, location=view.text_point(row,col))
+
+def open_url(href):
+    webbrowser.open(href)
